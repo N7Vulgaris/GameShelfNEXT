4b76fd0340e0375e80cd85a2635dd3ca572f67c6
Simen <wokiwoki@live.no>
2026-09-16 19:13:06 +0200

    Update for deployment

--- game-shelf/app/management/page.tsx
+++ game-shelf/app/management/page.tsx
@@ -1,6 +1,8 @@
 import GamesManager from "@/components/games-manager";
 import { getGames } from "@/lib/services/gameServices";
 
+export const dynamic = "force-dynamic";
+
 export default async function ManagementPage() {
   const games = await getGames();
 
--- game-shelf/app/viewGames/page.tsx
+++ game-shelf/app/viewGames/page.tsx
@@ -1,6 +1,8 @@
 import GamesManager from "@/components/games-manager";
 import { getGames } from "@/lib/services/gameServices";
 
+export const dynamic = "force-dynamic";
+
 export default async function ViewGamesPage() {
   const games = await getGames();
 
